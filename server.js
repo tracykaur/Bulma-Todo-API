9402d4f8588e29cf91035ab04e9954ab6e5bee05
tracykaur <tracykaur@yahoo.com>
2017-11-30 14:03:29 +1000

    added posting to the api

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
 const tasks = [
   { id : 1, todo:'Go for a run', time: '29/11/2017, 13:26:31', complete: true},
   { id : 2, todo:'Go for a walk', time: '29/11/2017, 13:26:31', complete: false}
 ];
 
+app.use(bodyParser.json());
 
 app.get('/api/tasks', (req, res) => res.send(tasks))
+
+app.post('/api/tasks', (req, res) =>{
+  console.log(req.body)
+  let task = req.body
+  tasks.push(task)
+  res.send(task)
+})
 
 
 app.listen(3001, () => console.log('Example app listening on port 3001!'))
